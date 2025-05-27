bd69fc46948346a4b528b53de77490aa26ef0dc0
Gregorio <karuzo.g@edu.mirea.ru>
2025-05-27 17:31:19 +0300

    Il mio commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/repetitor24/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
